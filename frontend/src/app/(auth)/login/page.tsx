45f0e76922033bc08fdfd5beb7603984e866987f
SamuDev <samudev01@gmail.com>
2024-11-19 12:43:17 -0300

    Screaming architecture

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginFormPage = () => {
+    return (
+        <div>LoginForm</div>
+    )
+}
+
+export default LoginFormPage
---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RegisterFormPage
+    = () => {
+        return (
+            <div>RegisterForm
+
+            </div>
+        )
+    }
+
+export default RegisterFormPage
