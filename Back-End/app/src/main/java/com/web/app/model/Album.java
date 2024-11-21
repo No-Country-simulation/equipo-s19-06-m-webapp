4f1ee62017ca18e86838ecebce3fabb7508fa9d4
Chenhao <chenhaohu555@gmail.com>
2024-11-21 08:23:25 +0100

    Updated models

--- Back-End/app/src/main/java/com/web/app/model/Album.java
+++ Back-End/app/src/main/java/com/web/app/model/Album.java
@@ -1,4 +1,23 @@
 package com.web.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Year;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Album {
+    @Id
+    private Long id;
+    private String nombre;
+    private Year anioLanzamiento;
 }
--- Back-End/app/src/main/java/com/web/app/model/Cancion.java
+++ Back-End/app/src/main/java/com/web/app/model/Cancion.java
@@ -1,4 +1,24 @@
 package com.web.app.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Cancion {
+    @Id
+    private long id;
+    private String nombre;
+    private int duracion;
+    private String genero;
+    private String urlImg;
+    @ManyToOne
+    private Album album;
 }
