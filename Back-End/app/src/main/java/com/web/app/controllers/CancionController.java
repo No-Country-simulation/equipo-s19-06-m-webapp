e562d1b9d593224f022d042b72c747802c5c561f
Chenhao <chenhaohu555@gmail.com>
2024-11-21 08:22:19 +0100

    Updated cancion controller

--- Back-End/app/src/main/java/com/web/app/controllers/CancionController.java
+++ Back-End/app/src/main/java/com/web/app/controllers/CancionController.java
@@ -1,4 +1,20 @@
 package com.web.app.controllers;
 
+import com.web.app.dto.CancionRequest;
+import com.web.app.service.CancionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/canciones")
+@RequiredArgsConstructor
 public class CancionController {
+
+    private final CancionService service;
+
+    @PostMapping
+    public void anadirCancion (@RequestBody CancionRequest request) {
+        service.anadirCancion(request);
+    }
+
 }
