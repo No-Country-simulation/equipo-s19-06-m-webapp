6d01f864acf062ca0406c09c727f1bebf2259432
Chenhao <chenhaohu555@gmail.com>
2024-11-21 08:24:20 +0100

    Updated services

--- Back-End/app/src/main/java/com/web/app/service/CancionService.java
+++ Back-End/app/src/main/java/com/web/app/service/CancionService.java
@@ -1,4 +1,26 @@
 package com.web.app.service;
 
+import com.web.app.dto.CancionAPIResponse;
+import com.web.app.mapper.CancionMapper;
+import com.web.app.model.Cancion;
+import com.web.app.repository.CancionRepository;
+import com.web.app.dto.CancionRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CancionService {
+
+    private final CancionRepository repository;
+    private final CancionMapper mapper;
+    private final DeezerApiClient deezerApiClient;
+
+    public void anadirCancion(CancionRequest request) {
+        if(!repository.existsById(request.id())) {
+            CancionAPIResponse cancionApi = deezerApiClient.buscarCancion(request.id());
+            Cancion cancion = mapper.toCancion(cancionApi);
+            repository.save(cancion);
+        }
+    }
 }
--- Back-End/app/src/main/java/com/web/app/service/DeezerApiClient.java
+++ Back-End/app/src/main/java/com/web/app/service/DeezerApiClient.java
@@ -1,4 +1,16 @@
 package com.web.app.service;
 
+import com.web.app.dto.CancionAPIResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class DeezerApiClient {
+
+    private String url = "https://api.deezer.com/";
+
+    public CancionAPIResponse buscarCancion(Long id) {
+        RestTemplate template = new RestTemplate();
+        return template.getForObject(url + "track/" + id, CancionAPIResponse.class);
+    }
 }
