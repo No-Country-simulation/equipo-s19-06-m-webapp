de994c5cf0005bacd05bfaf57b699f34da8750d1
Chenhao <chenhaohu555@gmail.com>
2024-11-21 08:22:49 +0100

    Updated cancion mapper

--- Back-End/app/src/main/java/com/web/app/mapper/CancionMapper.java
+++ Back-End/app/src/main/java/com/web/app/mapper/CancionMapper.java
@@ -1,4 +1,17 @@
 package com.web.app.mapper;
 
+import com.web.app.dto.CancionAPIResponse;
+import com.web.app.model.Cancion;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CancionMapper {
+
+    public Cancion toCancion(CancionAPIResponse response){
+        return Cancion.builder()
+                .id(response.id())
+                .nombre(response.title())
+                .duracion(response.duration())
+                .build();
+    }
 }
