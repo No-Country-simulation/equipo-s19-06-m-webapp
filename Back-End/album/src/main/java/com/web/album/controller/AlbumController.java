73c22a0685d739239f8f7ff360ad4c5816d7b603
Chenhao <chenhaohu555@gmail.com>
2024-11-25 07:21:14 +0100

    Added tags in controllers

--- Back-End/album/src/main/java/com/web/album/controller/AlbumController.java
+++ Back-End/album/src/main/java/com/web/album/controller/AlbumController.java
@@ -10,10 +10,12 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Albumes", description = "Gestionar todos los End-Points de albumes.")
 @RestController
 @RequestMapping("/albums")
 @RequiredArgsConstructor
